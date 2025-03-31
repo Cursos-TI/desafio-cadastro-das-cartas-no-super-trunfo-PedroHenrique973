fecf3b378af2936b3f35ef430304c15df783bf5f
PedroHenrique973 <boxpedrohenrique@gmail.com>
2025-03-31 19:04:48 +0000

    Codigo Prontoo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,8 +8,11 @@ int main(){
     char nome[] = "São Paulo";
     int populacao = 12325000;
     float area = 1521.1;
-    float pib = 699.28;
+    double pib = 699280000000;
     int numero = 50;
+    float quociente = populacao / area;
+    float percapita = pib / (float)populacao;
+    unsigned long long int poder = (populacao + area + pib + numero + quociente + (1 / percapita) ) / 6;
 
     //carta 2
     char estado2[] = "RJ";
@@ -17,8 +20,11 @@ int main(){
     char nome2[] = "Rio de Janeiro";
     int populacao2 = 6748000;
     float area2 = 1200.2;
-    float pib2 = 300.5;
+    double pib2 = 300500000000;
     int numero2 = 30;
+    float quociente2 = populacao2 / area2;
+    float percapita2 = pib2 / (float)populacao2;
+    unsigned long long int poder2 = (populacao2 + area2 + pib2 + numero2 + quociente2 + (1 / percapita2) ) / 6;
 
     //print da carta 1
     printf("Carta 1: \n\n");
@@ -27,10 +33,11 @@ int main(){
     printf("Nome da cidade: %s\n", nome);
     printf("População: %i\n", populacao);
     printf("Área em km²: %.1f\n", area);
-    printf("Pib: %.2f Bilhões\n", pib);
+    printf("PIB: %.0f\n", pib);
     printf("Numero de pontos Turisticos: %i\n", numero);
-
-    printf("\n"); //Espaço entre cartas
+    printf("Densidade Populacional: %.2f\n", quociente);
+    printf("PIB per capita: %.2f\n", percapita);
+    printf("Super Poder: %lli\n\n", poder);
 
     //pint da carta 2
     printf("Carta 2: \n\n");
@@ -39,8 +46,21 @@ int main(){
     printf("Nome da cidade: %s\n", nome2);
     printf("População: %i\n", populacao2);
     printf("Área em km²: %.1f\n", area2);
-    printf("Pib: %.1f Bilhões\n", pib2);
+    printf("PIB: %.f\n", pib2);
     printf("Numero de pontos Turisticos: %i\n", numero2);
+    printf("Densidade Populacional: %.2f\n", quociente2);
+    printf("PIB per capita: %.2f\n", percapita2);
+    printf("Super Poder: %lli\n\n", poder2);
+    
+
+    //comparação da carta 1 e da carta 2
+    printf("População da Carta 1 > Populção Carta 2: %d\n", populacao > populacao2);
+    printf("Área Carta 1 > Área Carta 2: %d\n", area > area2);
+    printf("PIB Carta 1 > PIB Carta 2: %d\n", pib > pib2);
+    printf("Pontos Turisticos Carta 1 > Pontos Turisticos Carta 2: %d\n", numero > numero2);
+    printf("Densidade Populacional 1 > Densidade Populacional Carta 2: %d\n", quociente > quociente2);
+    printf("PIB per capita 1 > PIB per capita Carta 2: %d\n", percapita < percapita2);
+    printf("Super Poder 1 > Super Poder Carta 2: %d\n", poder > poder2);
 
     return 0;
 }
